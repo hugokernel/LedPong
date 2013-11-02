3180c505006f81f7d6c67112578153d8e4353866
hugokernel <hugo@digitalspirit.org>
2013-11-02 15:00:45 +0000

    Raspy modif

--- raspberry/test.py
+++ raspberry/test.py
@@ -9,7 +9,7 @@ serial = serial.Serial(SERIAL_DEV, 115200, timeout = 1)
 CMD_LED_COLOR = 1
 CMD_BRIGHTNESS = 2
 
-def sendCmd(index, color):
+def sendCmd(index, cmd, args):
     
 
 for index in range(0, 32)
